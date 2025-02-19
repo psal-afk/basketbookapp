b85917fee4e2e0b0edc7eb5bca9bc2f670200693
psal-afk <p_salimo@fanshaweonline.ca>
2025-02-19 00:21:35 -0500

    initial commits

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./components/Home";
 import MyBookings from "./components/MyBookings";
+import Book from "./components/Book";
 import Contact from "./components/Contact";
 import images from "./images/img-2.jpg";
 import "./styles/Home.css";
@@ -23,6 +24,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/my-bookings" element={<MyBookings />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/book" element={<Book />} />
         </Routes>
       </Router>
     </div>
---
+++ src/components/Book.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Book = () => {
+  return (
+    <div className="book">
+      <h1>Book </h1>
+      <p>BOOK NOW</p>
+    </div>
+  );
+};
+
+export default Book;
