2602a22f773e5432bb597810025339893a944666
psal-afk <p_salimo@fanshaweonline.ca>
2025-02-19 02:29:07 -0500

    created login page

---
+++ src/components/AdminLogin.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "../styles/AdminLogin.css";
+import { FaUser, FaLock } from "react-icons/fa";
+
+const AdminLogin = () => {
+  return (
+    <div className="main">
+      <div className="wrapper">
+        <form action={""}>
+          <h1 style={{ color: "white" }}>Admin Login</h1>
+          <div className="input-box">
+            <input type="text" placeholder="Username" required />
+            <FaUser style={{ color: "white" }} className="icon" />
+          </div>
+          <div className="input-box">
+            <input type="password" placeholder="Password" required />
+            <FaLock style={{ color: "white" }} className="icon" />
+          </div>
+          <div className="remember-forgot">
+            <label style={{ color: "white" }}>
+              <input type="checkbox" />
+              Remember me
+            </label>
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+export default AdminLogin;
